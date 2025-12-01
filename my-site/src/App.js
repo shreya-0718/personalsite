2703afbb606e6771aa0d85f4dc7a71656ac8687c
Shreya <153142441+shreya-0718@users.noreply.github.com>
2025-11-30 22:10:13 -0600

    updated main page with theme, added custom cursor

--- my-site/src/App.js
+++ my-site/src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import Cafe from "./components/Cafe";
+import Cursor from "./components/Cursor";
 
 function App() {
+  useEffect(() => {
+    const handleEnter = (e) => {
+      if (e.target.tagName === "BUTTON") {
+        document.body.classList.add("cursor-glow");
+      }
+    };
+
+    const handleLeave = (e) => {
+      if (e.target.tagName === "BUTTON") {
+        document.body.classList.remove("cursor-glow");
+      }
+    };
+
+    document.addEventListener("mouseover", handleEnter);
+    document.addEventListener("mouseout", handleLeave);
+
+    return () => {
+      document.removeEventListener("mouseover", handleEnter);
+      document.removeEventListener("mouseout", handleLeave);
+    };
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Cursor/>
+      <Cafe/>
     </div>
   );
 }
---
+++ my-site/src/components/Cafe.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Cafe.css'
+
+function Cafe() {
+  return (
+    <>
+        <div className='cafe-wrapper'>
+            <div className='ye-shall-float'>
+                <img src="cat_cafe.jpg" className="cafe" alt="" />
+
+                <button className="sign"></button>
+            </div>
+            
+        
+
+            
+
+        </div>
+
+    </>
+  )
+}
+
+export default Cafe
